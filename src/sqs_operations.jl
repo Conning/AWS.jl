555e9ca6a0e5a82f94902ac5e153f88990f81131
Keith Mason <keith.mason@conning.com>
2015-10-04 18:50:13 -0500

    implement GetQueueUrl and ListQueues

--- src/sqs_operations.jl
+++ src/sqs_operations.jl
@@ -32,7 +32,43 @@ end
 export DeleteQueue
 
 
+function GetQueueUrl(env::AWSEnv, msg::GetQueueUrlType)
+    sqsresp::SQSResponse = call_sqs(env, "GetQueueUrl" , msg)
+    if  (sqsresp.pd != nothing) && (sqsresp.obj == nothing)
+        sqsresp.obj = GetQueueUrlResponseType(sqsresp.pd)
+    end
+    sqsresp
+end
+function GetQueueUrl(env::AWSEnv; kwargs...)
+    msg=GetQueueUrlType()
+    for p in kwargs
+        setfield!(msg, p[1], p[2])
+    end
+    GetQueueUrl(env, msg)
+end
+export GetQueueUrl
+
+
+function ListQueues(env::AWSEnv, msg::ListQueuesType)
+    sqsresp::SQSResponse = call_sqs(env, "ListQueues" , msg)
+    if  (sqsresp.pd != nothing) && (sqsresp.obj == nothing)
+        sqsresp.obj = ListQueuesResponseType(sqsresp.pd)
+    end
+    sqsresp
+end
+function ListQueues(env::AWSEnv; kwargs...)
+    msg=ListQueuesType()
+    for p in kwargs
+        setfield!(msg, p[1], p[2])
+    end
+    ListQueues(env, msg)
+end
+export ListQueues
+
+
 ValidRqstMsgs = Dict(
     "CreateQueueType"=>true,
+    "GetQueueUrlType"=>true,
+    "ListQueuesType"=>true,
     "DeleteQueueType"=>true
 )
--- src/sqs_types.jl
+++ src/sqs_types.jl
@@ -30,6 +30,72 @@ end
 export CreateQueueResponseType
 
 
+type GetQueueUrlType
+    queueName::Union{ASCIIString, Void}
+    queueOwnerAWSAccountId::Union{ASCIIString, Void}
+
+    GetQueueUrlType(; queueName=nothing, queueOwnerAWSAccountId=nothing) =
+         new(queueName, queueOwnerAWSAccountId)
+end
+function GetQueueUrlType(pd::ETree)
+    o = GetQueueUrlType()
+    o.queueName = LibExpat.find(pd, "queueName#string")
+    o.queueOwnerAWSAccountId = LibExpat.find(pd, "queueOwnerAWSAccountId#string")
+    o
+end
+
+export GetQueueUrlType
+
+
+type GetQueueUrlResponseType
+    queueUrl::Union{ASCIIString, Void}
+    requestId::Union{ASCIIString, Void}
+
+    GetQueueUrlResponseType(; queueUrl=nothing, requestId=nothing) =
+         new(queueUrl, requestId)
+end
+function GetQueueUrlResponseType(pd::ETree)
+    o = GetQueueUrlResponseType()
+    o.queueUrl = LibExpat.find(pd, "GetQueueUrlResult/QueueUrl#string")
+    o.requestId = LibExpat.find(pd, "ResponseMetadata/RequestId#string")
+    o
+end
+
+export GetQueueUrlResponseType
+
+
+type ListQueuesType
+    queueNamePrefix::Union{ASCIIString, Void}
+
+    ListQueuesType(; queueNamePrefix=nothing) =
+         new(queueNamePrefix)
+end
+function ListQueuesType(pd::ETree)
+    o = ListQueuesType()
+    o.queueName = LibExpat.find(pd, "queueNamePrefix#string")
+    o
+end
+
+export ListQueuesType
+
+
+type ListQueuesResponseType
+    queueUrlSet::Union{Vector{ASCIIString}, Void}
+    requestId::Union{ASCIIString, Void}
+
+    ListQueuesResponseType(; queueUrlSet=nothing, requestId=nothing) =
+         new(queueUrlSet, requestId)
+end
+function ListQueuesResponseType(pd::ETree)
+    o = ListQueuesResponseType()
+    o.queueUrlSet = ASCIIString[url.elements[1] for url in LibExpat.find(pd, "ListQueuesResult/QueueUrl")]
+    o.requestId = LibExpat.find(pd, "ResponseMetadata/RequestId#string")
+    o
+end
+
+export ListQueuesResponseType
+
+
 type DeleteQueueType
     queueUrl::Union{ASCIIString, Void}
 
