73a9b9cc233e1c10e39fde2fd9e69a824b79299a
Keith Mason <keith.mason@conning.com>
2015-10-09 11:44:24 -0500

    add SendMessage and GetQueueAttribute tests

--- test/testsqs.jl
+++ test/testsqs.jl
@@ -30,6 +30,23 @@ try
         println("ERROR : Can't find new queue in response from ListQueues")
         dump(resp.queueUrlSet)
     end
+
+    msgAttributes = MessageAttributeType[]
+    push!(msgAttributes, MessageAttributeType(name="some-attribute",
+      value=MessageAttributeValueType(dataType="Number.subtype", stringValue="0")))
+    push!(msgAttributes, MessageAttributeType(name="other-attribute",
+      value=MessageAttributeValueType(dataType="String.yyy", stringValue="My yyy string")))
+    push!(msgAttributes, MessageAttributeType(name="bin-attribute",
+      value=MessageAttributeValueType(dataType="Binary.jpg", binaryValue=[0x12,0x19,0x1e,0x11,0x22,0x19])))
+    SendMessage(env; queueUrl=qurl, delaySeconds=5, messageBody="test", messageAttributeSet=msgAttributes)
+
+    resp = GetQueueAttributes(env, queueUrl=qurl, attributeNameSet=["ApproximateNumberOfMessagesDelayed"]).obj
+    if isa(resp, SQSError)
+        throw(resp)
+    end
+    if parse(Int,resp.attributeSet[1].value) != 1
+        println("ERROR : Expected one delayed message")
+    end
 catch e
     println("ERROR: $e")
 end
